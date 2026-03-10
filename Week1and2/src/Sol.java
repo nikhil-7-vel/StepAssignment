6fc955385cbf7b4c164f3b86a6e73efbc0969ff9
nikhil-7-vel <nikhilvelayudham07@gmail.com>
2026-03-10 09:14:59 +0530

    E-commerce Flash Sale Inventory Manager

--- Week1and2/src/Sol.java
+++ Week1and2/src/Sol.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-public class Sol {
-}
+public class FlashSale {
+
+    static HashMap<String,Integer> stock = new HashMap<>();
+    static Queue<Integer> waitingList = new LinkedList<>();
+
+    static void checkStock(String product){
+        System.out.println("Stock: "+stock.get(product));
+    }
+
+    static void purchaseItem(String product,int userId){
+
+        if(stock.get(product)>0){
+            stock.put(product, stock.get(product)-1);
+            System.out.println("Purchase successful. Remaining: "+stock.get(product));
+        }
+        else{
+            waitingList.add(userId);
+            System.out.println("Out of stock. Added to waiting list position "+waitingList.size());
+        }
+    }
+
+    public static void main(String[] args){
+
+        stock.put("IPHONE15_256GB",100);
+
+        checkStock("IPHONE15_256GB");
+
+        purchaseItem("IPHONE15_256GB",12345);
+        purchaseItem("IPHONE15_256GB",67890);
